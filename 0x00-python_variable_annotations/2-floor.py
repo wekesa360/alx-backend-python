3c6863535ea08fce900ab4521bd45cfa62cabb7c
Michael Wekesa <wekesa360@outlook.com>
2022-04-20 17:39:59 +0300

    update: complete task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""a typed-annotated function floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float number
+    """
+    return math.floor(n)
