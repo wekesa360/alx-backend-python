01e84ca4c0fc5b8dd9fe2d39d614873521ac7ea2
Michael Wekesa <wekesa360@outlook.com>
2022-04-20 17:39:39 +0300

    update: complete task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a type-annotated function concat
+"""
+
+def concat(str1: str, sr2: str) -> str:
+    """returns a concatenation of two strings
+    """
+
+    return "{}{}".format(str1, sr2)
